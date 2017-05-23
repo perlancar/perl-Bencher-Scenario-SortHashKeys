7298a2812028fd55c0ef10e426d8004b9bfa4d98
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2017-05-24 06:32:45 +0700

    Initial commit

---
+++ lib/Bencher/Scenario/SortHashKeys.pm
@@ -0,0 +1,33 @@
+package Bencher::Scenario::SortHashKeys;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+our $scenario = {
+    summary => 'Benchmark Sort::HashKeys',
+    participants => [
+        {
+            name => 'map',
+            code_template => 'state $h = <hash>; map {($_, $h->{$_})} sort keys %$h',
+            result_is_list => 1,
+        },
+        {
+            module => 'Sort::HashKeys',
+            function => 'sort',
+            code_template => 'state $h = <hash>; Sort::HashKeys::sort(%$h)',
+            result_is_list => 1,
+        },
+    ],
+    datasets => [
+        {name=>'2key'  , args=>{hash=>{map {$_=>1} 1..  2}} },
+        {name=>'10key' , args=>{hash=>{map {$_=>1} 1.. 10}} },
+        {name=>'100key', args=>{hash=>{map {$_=>1} 1..100}} },
+    ],
+};
+
+1;
+# ABSTRACT:
